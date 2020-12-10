65fc9fb2db69d3f59e7620baf6609fb079ef8cf4
shs-zhcsyfczyjhy <tzword@163.com>
2020-12-10 14:05:50 +0800

    消息总线

--- config-client/src/main/java/com/tzword/configclient/controller/ConfigController.java
+++ config-client/src/main/java/com/tzword/configclient/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.tzword.configclient.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
+@RefreshScope
 @RequestMapping("/configTest")
 public class ConfigController {
 
     @Value("${foo}")
     String foo;
 
+    /**
+     * @Description: 首先在配置类加注解@RefreshScope（不是启动类）
+     * 另外springboot 2.0之后除了暴露了health和info端点外，其他端点均不能访问 可以在配置文件中添加以下配置来解决： management: endpoints: web: exposure: include: "*"(这里也可以是["bus-refresh"])
+     * 另外，配置后POST请求更改为：http://ip:port/actuator/bus-refresh
+     * @param  1
+     * @return java.lang.String
+     * @throws
+     * @author jianghy
+     * @date 2020/12/10 14:04
+     */
     @RequestMapping("/getConfigInfo")
     public String getConfigInfo(){
         log.info("获取到配置");
